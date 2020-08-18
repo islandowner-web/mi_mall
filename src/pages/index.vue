1da743bf7c9fe6b601531112dd0c46968de44d99
islandowner-web <949172252@qq.com>
2020-08-19 01:09:41 +0800

    路由封装

---
+++ src/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Index
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'index',
+  components: {
+
+  }
+}
+</script>
---
+++ src/pages/oederList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    oederList
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'oeder-list',
+  components: {
+
+  }
+}
+</script>
